78b8b011e9decc8fa057536320cfe3ce4ccb9805
PoojithaYadavalli <poojithareddyyadavalli@gmail.com>
2020-07-08 13:32:31 +0530

    Update solution.py

--- solution.py
+++ solution.py
@@ -1,3 +1,32 @@
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+        
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+        
+def linkedList(llist):
+    if len(llist)==0:
+        return None
+    head1=head=ListNode(llist[0])
+    for i in range(1,len(llist)):
+        head.next=ListNode(llist(i))
+        head=head.next
+    head.next=None
+    return head1
+
+def binaryTree(arr, root, i, n):
+    if i < n: 
+        temp = TreeNode(arr[i])  
+        root = temp 
+        root.left = insertLevelOrder(arr, root.left, 2 * i + 1, n) 
+        root.right = insertLevelOrder(arr, root.right, 2 * i + 2, n)
+    return root
+    
 def isSubPath(head, root) :
     target = ""
     while head:
@@ -26,4 +55,7 @@ def isSubPath(head, root) :
     
 tree = input.split(',')
 llist= input().split(',')
-print(isSubPath(head,root))
+head = linkedList(llist)
+root1=None
+root=binaryTree(tree, root1,0,len(tree))
+print(isSubPath(header,root))
